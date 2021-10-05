104311065722e5be3b57ea14c8d59f80b6e3dece
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-10-05 19:57:13 -0400

    Updates

---
+++ precache-manifest.5d972059324e9acc4b78d1e7afe91ac7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ec9c39afc54aca419938620aaa7e5a",
+    "url": "/git@github.com:slongy7/react-portfolio.git/index.html"
+  },
+  {
+    "revision": "abda88ea1c3fddeff657",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/css/main.a3592a7f.chunk.css"
+  },
+  {
+    "revision": "ccdf87bc2252e80d8048",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/js/2.6cf05abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/js/2.6cf05abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abda88ea1c3fddeff657",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/js/main.534b83a5.chunk.js"
+  },
+  {
+    "revision": "73cd176206b71930c52b",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/js/runtime-main.e564b729.js"
+  },
+  {
+    "revision": "3921d6a992b9dec4b4b6a56f854b4d19",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/media/Headshot.3921d6a9.jpeg"
+  },
+  {
+    "revision": "a39808f9a4e5fd33eb09b200de53ac77",
+    "url": "/git@github.com:slongy7/react-portfolio.git/static/media/resume_justin_long.a39808f9.pdf"
+  }
+]);
